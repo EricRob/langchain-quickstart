013892df1dcfbaaa3b2c50dada299c4eac15f1f2
Eric Robinson <eric.james.robinson@gmail.com>
2024-05-09 04:34:47 +0000

    Initial edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain.llms import OpenAI
-st.set_page_config(page_title="🦜🔗 Quickstart App")
-st.title('🦜🔗 Quickstart App')
+st.set_page_config(page_title="Anki Automatoton")
+st.title('Anki Automatoton')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
@@ -9,10 +9,18 @@ def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   st.info(llm(input_text))
 
+col1, col2 = st.columns(2)
+
+with col1:
+    med_text = st.text_input("Source Text")
+
+with col2:
+    inst_text = st.text_input("Instruction Text")
+
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
+    st.warning('Please enter the OpenAI API key!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
     generate_response(text)
